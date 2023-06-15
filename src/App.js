c3abdaf07aa877ebc796988578c4761242aae2e5
Kartik Ohri <kartikohri13@gmail.com>
2023-06-15 16:40:38 +0530

    Update sample.json, add tracks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Card from './Card';
-import data from './sample.json';
+import data from './data/sample.json';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <div className="row">
         {data.map(item => (
             <div className="col-4" style={{marginBottom: "18px"}} key={item.id}>
-              <Card track={item.track} artist={item.artist} image={item.image} url={item.url} id={item.id} />
+              <Card album={item.album} artist={item.artist} image={item.image} url={item.url} id={item.id} />
             </div>
         ))}
       </div>
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,9 @@
-function Card({track, artist, image, url, id}) {
+function Card({album, artist, image, url, id}) {
     return (
         <div className="card" style={{width: "18rem"}}>
-            <img src={image} className="card-img-top" alt={`Album art of ${track}`}/>
+            <img src={image} className="card-img-top" alt={`Album art of ${album}`}/>
             <div className="card-body">
-                <h5 className="card-title">{track}</h5>
+                <h5 className="card-title">{album}</h5>
                 <p className="card-text">{artist}</p>
                 <a href={url} className="btn btn-primary">Listen on Spotify</a>
             </div>
