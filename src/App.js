1e5e5994e059359d4a984aec9d58bda079be9ccf
Kartik Ohri <kartikohri13@gmail.com>
2023-06-17 12:57:14 +0530

    Add track card to display track list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import ReleaseCard from './ReleaseCard';
 import data from './data/sample.json';
+import Track from "./Track";
+import {useState} from "react";
 
 function App() {
+  const [tracks, setTracks] = useState([]);
+
   return (
     <div className="container">
-      <div className="row">
-        {data.map(item => (
-            <div className="col-4" style={{marginBottom: "18px"}} key={item.id}>
-              <ReleaseCard album={item.album} artist={item.artist} image={item.image} url={item.url} id={item.id} />
+        <div className="row">
+            <div className="col-9">
+                <div className="row">
+                    {data.map(item => (
+                        <div className="col-4" style={{marginBottom: "18px"}} key={item.id} onClick={() => setTracks(item["tracks"])}>
+                            <ReleaseCard album={item.album} artist={item.artist} image={item.image} url={item.url} id={item.id} />
+                        </div>
+                    ))}
+                </div>
             </div>
-        ))}
-      </div>
+            <div className="col-3">
+                <div className="row">
+                    {tracks.map(item => (
+                        <Track artist={item.artist} url={item.url} id={item.id} preview_url={item.preview_url} duration_ms={item.duration_ms} name={item.name} />
+                    ))}
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/Track.js
@@ -0,0 +1,13 @@
+function Track({name, id, url, preview_url, duration_ms, artist}) {
+    return (
+        <a key={id} href={url} className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">{name}</h5>
+                {/*<small className="text-body-secondary">3 days ago</small>*/}
+            </div>
+            <p className="mb-1">{artist}</p>
+        </a>
+    );
+}
+
+export default Track;
