e598e80ed90e7f8b2db820bb96d52283ba98135b
Kartik Ohri <kartikohri13@gmail.com>
2023-06-17 22:18:10 +0530

    Integrate with spotify API

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
 import './App.css';
 import ReleaseCard from './ReleaseCard';
-import data from './data/sample.json';
 import Track from "./Track";
 import {useEffect, useState} from "react";
 
 //
 function App() {
+  const [releases, setReleases] = useState([]);
   const [tracks, setTracks] = useState([]);
+  const [selectedAlbum, setSelectedAlbum] = useState();
+
+  const token = "BQDgBd3drCl7Ka2N_VwPyr6DB3N44htpuu4UZJ45uH_JDAa0zirD3CZA8I4f0-KsMFFo5YIFjADIuNybDP1MtrR76s00n5zuneLFlXPtkEdkxG3M4T6VER-a5pkm7lm8DC8-sSG5Nx6Lr0A8iekqOOoDf_DwV21axz1Tdwgm60PT9opEtkuXtpMny5zSYn7SVOSiqIMztB26Iic";
 
   useEffect( () => {
       async function fetchData() {
           const response = await fetch("https://api.spotify.com/v1/browse/new-releases", {
               headers: {
-                  "Authorization": "Bearer BQBCwr2DpC4UMMLVQjukXZaKvn22LTUKnf-lvkWXfIDwxuwBeSqbFkZJwJnPx53FOPXV7-hqyqFYvU6i0nRt6IxU70Qj9fQtGT5RTy3_0T_y4Xxljss-Fz6Ftuo0j70gaVDKg6pSJ5EevmGvqSHiKZW_RmFaeaxDWWhL4zMI_ed8UgIgSchWmoJWJLP0SHj6veXd3VGFNs41QHk"
+                  "Authorization": `Bearer ${token}`
               }
           });
-          const albums = await response.json();
-          console.log(albums);
+          const data = await response.json();
+          const albums = data["albums"]["items"];
+          setReleases(albums);
       }
       fetchData();
   }, []);
+
+  useEffect(() => {
+      if (!selectedAlbum) {
+          return;
+      }
+
+      async function fetchData() {
+          const albumId = selectedAlbum.id;
+          const response = await fetch(`https://api.spotify.com/v1/albums/${albumId}/tracks`, {
+              headers: {
+                  "Authorization": `Bearer ${token}`
+              }
+          });
+          const data = await response.json();
+          const tracks = data["items"];
+          setTracks(tracks);
+      }
+      fetchData();
+  }, [selectedAlbum]);
 
   return (
     <div className="container">
         <div className="row">
             <div className="col-9">
                 <div className="row">
-                    {data.map(item => (
-                        <div className="col-4" style={{marginBottom: "18px"}} key={item.id} onClick={() => setTracks(item["tracks"])}>
-                            <ReleaseCard album={item.album} artist={item.artist} image={item.image} url={item.url} id={item.id} />
+                    {releases.map(item => (
+                        <div className="col-4" style={{marginBottom: "18px"}} key={item.id} onClick={() => setSelectedAlbum(item)}>
+                            <ReleaseCard album={item.name} artist={item["artists"][0]["name"]} image={item.images[0]["url"]} url={item["external_urls"]["spotify"]} id={item.id} />
                         </div>
                     ))}
                 </div>
@@ -36,7 +59,7 @@ function App() {
             <div className="col-3">
                 <div className="row">
                     {tracks.map(item => (
-                        <Track artist={item.artist} url={item.url} id={item.id} preview_url={item.preview_url} duration_ms={item.duration_ms} name={item.name} />
+                        <Track artist={item["artists"][0]["name"]} url={item["external_urls"]["spotify"]} id={item.id} preview_url={item.preview_url} duration_ms={item.duration_ms} name={item.name} />
                     ))}
                 </div>
             </div>
