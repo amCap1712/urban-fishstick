c09c589bc08cc26cdcded3a17ceaa84cddec3c6e
Kartik Ohri <kartikohri13@gmail.com>
2023-06-22 20:37:59 +0530

    Make cards equal sized

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [tracks, setTracks] = useState([]);
   const [selectedAlbum, setSelectedAlbum] = useState();
 
-  const token = "BQDgBd3drCl7Ka2N_VwPyr6DB3N44htpuu4UZJ45uH_JDAa0zirD3CZA8I4f0-KsMFFo5YIFjADIuNybDP1MtrR76s00n5zuneLFlXPtkEdkxG3M4T6VER-a5pkm7lm8DC8-sSG5Nx6Lr0A8iekqOOoDf_DwV21axz1Tdwgm60PT9opEtkuXtpMny5zSYn7SVOSiqIMztB26Iic";
+  const token = "BQC0F1qnCPmwSQjnOtHzgZYRSVosEwsn1DNIhWnbzWl-OkQ2SG4HBdYEb8gewDg75eAIJDo1_PG_erfTzxeQMLqT-NFXGPOBP87_KSQCmpPjhpvtjA7Q6dYZ31UsOoLpg4olfszeiqz-Xp6aXodYhozuvdz8bwf0Jci7GsdZSS1FK7yWtxJxC_nR5WHgwPApW0Y563N5nIBa7aM";
 
   useEffect( () => {
       async function fetchData() {
@@ -48,7 +48,7 @@ function App() {
     <div className="container">
         <div className="row">
             <div className="col-9">
-                <div className="row">
+                <div className="row d-flex flex-wrap">
                     {releases.map(item => (
                         <div className="col-4" style={{marginBottom: "18px"}} key={item.id} onClick={() => setSelectedAlbum(item)}>
                             <ReleaseCard album={item.name} artist={item["artists"][0]["name"]} image={item.images[0]["url"]} url={item["external_urls"]["spotify"]} id={item.id} />
--- src/ReleaseCard.js
+++ src/ReleaseCard.js
@@ -1,10 +1,12 @@
 function ReleaseCard({album, artist, image, url, id}) {
     return (
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{width: "18rem", height: "100%"}}>
             <img src={image} className="card-img-top" alt={`Album art of ${album}`}/>
-            <div className="card-body">
-                <h5 className="card-title">{album}</h5>
-                <p className="card-text">{artist}</p>
+            <div className="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 className="card-title">{album}</h5>
+                    <p className="card-text">{artist}</p>
+                </div>
                 <a href={url} className="btn btn-primary">Listen on Spotify</a>
             </div>
         </div>
